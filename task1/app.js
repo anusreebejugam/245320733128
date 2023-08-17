c031cf392eee8e98121161c4e4370e08b476e381
Anusree Bejugam <anusree.bejugam2111@gmail.com>
2023-08-17 20:08:52 +0530

    task1 second commit

--- task1/app.js
+++ task1/app.js
@@ -7,6 +7,7 @@ const indexRouter = require('./routes/index');
 const trainsRouter = require('./routes/trains');
 
 const app = express();
+const PORT = 4000;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,4 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/trains', trainsRouter);
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
