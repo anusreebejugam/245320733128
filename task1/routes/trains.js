3ab1d773f870aa2939f77febd288b91be2664976
Anusree Bejugam <anusree.bejugam2111@gmail.com>
2023-08-17 20:14:22 +0530

    task1 fourth commit

--- task1/routes/trains.js
+++ task1/routes/trains.js
@@ -5,12 +5,14 @@ const getJWT = require('../util/getJWT');
 router.get('/', async function (req, res, next) {
   const token = await getJWT()
   const response = await fetch('http://20.244.56.144/train/trains', {
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`
     }
-  })
-  console.log(response);
-  res.send(response)
+  }).then(res => res.body)
+  const data = await response.getReader().read()
+  console.log(data);
+  res.send(data)
 });
 
 module.exports = router;
